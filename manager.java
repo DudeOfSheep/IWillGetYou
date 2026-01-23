5e5359243822c0b337681a4edaa91d34ce69d7cc
DeepShude <kashti.pickle@gmail.com>
2026-01-23 09:57:39 -0600

    Additional testing code

--- manager.java
+++ manager.java
@@ -2,5 +2,10 @@ public class manager {
   public static void main(String[] args) {
     Playlist Teto = new Playlist(6, "Teto!!!");
     System.out.println(Teto);
+
+    Teto.append("Teto Territory", "Cegook", 228, 2000000000);
+    Teto.append("Bird Brain", "Jamie Page", 256, 8156);
+
+    Teto.printSongList();
   }
 }
